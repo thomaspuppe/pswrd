e8f036bf2915a1b4e96b6c624f436640bc7c5443
thomaspuppe <info@thomaspuppe.de>
2020-12-03 21:37:28 +0100

    Add tabs as nav items

--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,47 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<ul class="tabs">
+		<li class="active">🐴 Words</li>
+		<li>🎲 Dice</li>
+		<li>Easy</li>
+		<li>Hard</li>
+	</ul>
+
+	<h1>Password Generator</h1>
+	<p>Easy password generation for {{ name }}</p>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 900px;
 		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	/* TABS ******************************** */
+	.tabs {
+		padding: 30px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		background-color: #f0f6f8;
+		list-style: none;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.tabs li {
+		cursor: pointer;
+		color: #0e1635;
+		border-radius: 5px;
+		padding: 10px;
+		text-align: center;
+		width: 100%;
+		margin-right: 5px;
+		background-color: #f0f6f8;
+		font-weight: 700;
+		transition: box-shadow 0.3s;
+	}
+	.tabs li.active {
+		box-shadow: 2px 2px 5px 0px rgba(138, 133, 133, 0.75);
 	}
 </style>
